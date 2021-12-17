290ba167ff4e50d12eaccc74d4db0bc6a251c148
joniumGit <52005121+joniumGit@users.noreply.github.com>
2021-12-17 11:26:48 +0200

    test server

---
+++ src/packet_buddy/server/__main__.py
@@ -0,0 +1,10 @@
+from ..base import IPMessager, Data, Message
+
+m = IPMessager[Data](69, b'super duper secret key! Encrypt!', 16)
+
+
+def on_message(message: Message):
+    m.message[Data(sender="server", topic="server reply", content=message.payload.content)] >> message.target
+
+
+m.receive(on_message)
